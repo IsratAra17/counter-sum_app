a9ec0d2933a6561e1b52b68b0b27c7c7706c0ab2
s.m.israt <smisratara25@gmail.com>
2023-05-06 08:07:39 +0600

    added two funtions,completed the sum app

--- lib/counter_page.dart
+++ lib/counter_page.dart
@@ -10,44 +10,99 @@ class Counter_page extends StatefulWidget {
 }
 
 class _Counter_pageState extends State<Counter_page> {
-  int countNumber=0;
+  int countNumber = 0;
+
+  Map<String,double> formValue = {"Num1": 0, "Num2": 0, "Num3": 0};
+  double sum=0;
   @override
   Widget build(BuildContext context) {
+
+    MyInputChange(Inputkey, InputValue) {
+      setState(() {
+        formValue.update((Inputkey), (value) => double.parse(InputValue));
+        print(formValue);
+      });
+    }
+AddAllNum(){
+      setState(() {
+        sum=formValue['Num1']!+formValue['Num2']!+formValue['Num3']!;
+        print(sum);
+      });
+}
     return Scaffold(
       appBar: AppBar(
         title: Text("Counter App"),
       ),
-      body:Padding(
-        padding: EdgeInsets.all(40),
+      body: Padding(
+        padding: EdgeInsets.all(30),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Text("Sum is:",style: AppTextStyle(),),
-            SizedBox(height: 22,width: double.infinity,),
-            TextField(decoration: AppInputStyle("First Number"),),
-            SizedBox(height: 22,width: double.infinity,),
-            TextField(decoration: AppInputStyle("Second Number"),),
-            SizedBox(height: 22,width: double.infinity,),
-            Container(width:double.infinity,child: ElevatedButton(onPressed: (){},style: AppBtnStyle(), child: Text("ADD",style: TextStyle(color:Colors.black),))),
-
-
-            SizedBox(height: 40,width: double.infinity,),
-            Text(countNumber.toString(),style:AppTextStyle()),
-
+            Text(
+              sum.toString(),
+              style: AppTextStyle(),
+            ),
+            SizedBox(
+              height: 22,
+              width: double.infinity,
+            ),
+            TextField(
+              onChanged: (value) {
+                MyInputChange("Num1", value);
+              },
+              decoration: AppInputStyle("First Number"),
+            ),
+            SizedBox(
+              height: 22,
+              width: double.infinity,
+            ),
+            TextField(
+              onChanged: ((value) {
+                MyInputChange("Num2", value);
+              }),
+              decoration: AppInputStyle("Second Number"),
+            ),
+            SizedBox(
+              height: 22,
+              width: double.infinity,
+            ),
+            TextField(
+              onChanged: ((value) {
+                MyInputChange("Num3", value);
+              }),
+              decoration: AppInputStyle("Third Number"),
+            ),
+            SizedBox(
+              height: 22,
+              width: double.infinity,
+            ),
+            Container(
+                width: double.infinity,
+                child: ElevatedButton(
+                    onPressed: () {
+                      AddAllNum();
+                    },
+                    style: AppBtnStyle(),
+                    child: Text(
+                      "ADD",
+                      style: TextStyle(color: Colors.black),
+                    ))),
+            // SizedBox(
+            //   height: 40,
+            //   width: double.infinity,
+            // ),
+            // Text(countNumber.toString(), style: AppTextStyle()),
           ],
         ),
-
       ),
-
-      floatingActionButton: FloatingActionButton(
-    child: Icon(Icons.add),
-    onPressed: (){
-    setState(() {
-    countNumber=countNumber+1;
-    });
-    },
-
-    ),
+      // floatingActionButton: FloatingActionButton(
+      //   child: Icon(Icons.add),
+      //   onPressed: () {
+      //     setState(() {
+      //       countNumber = countNumber + 1;
+      //     });
+      //   },
+      // ),
     );
   }
 }
